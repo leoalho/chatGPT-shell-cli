47249ad1dfc5b99ca88faaa7facf2260a32b06bc
Leo Alho <46094014+leoalho@users.noreply.github.com>
2024-03-20 21:46:05 +0200

    Add new command - current_model. Fix typo in README (#2)

--- chatgpt.sh
+++ chatgpt.sh
@@ -30,6 +30,7 @@ Commands:
   image: - To generate images, start a prompt with image: If you are using iTerm, you can view the image directly in the terminal. Otherwise the script will ask to open the image in your browser.
   history - To view your chat history
   models - To get a list of the models available at OpenAI API
+  current_model - To get the name of the current model
   model: - To view all the information on a specific model, start a prompt with model: and the model id as it appears in the list of models. For example: "model:text-babbage:001" will get you all the fields for text-babbage:001 model
   change_model: - To change the used model. For example "change_model:gpt-4" will change the current model to gpt-4
   command: - To get a command with the specified functionality and run it, just type "command:" and explain what you want to achieve. The script will always ask you if you want to execute the command. i.e.
@@ -375,6 +376,8 @@ while $running; do
 		model_data=$(echo $models_response | jq -r -C '.data[] | select(.id=="'"${prompt#*model:}"'")')
 		echo -e "$OVERWRITE_PROCESSING_LINE"
 		echo -e "${CHATGPT_CYAN_LABEL}Complete details for model: ${prompt#*model:}\n ${model_data}"
+	elif [[ "$prompt" == "current_model" ]]; then
+	  echo "$MODEL"
 	elif [[ "$prompt" =~ ^change_model: ]]; then
     		MODEL=${prompt#*change_model:}
 	elif [[ "$prompt" =~ ^command: ]]; then
